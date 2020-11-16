92412e69ebb63351107c2c4dab23ea0d2ce0163b
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-11-16 14:28:59 +0530

    Update healthApp.sol

--- healthApp.sol
+++ healthApp.sol
@@ -3,14 +3,19 @@ pragma experimental ABIEncoderV2;
 
 contract HealthApp {
 
-  string name;
-  uint age;
-  uint height;
-  uint weight;
-  uint bmi;
-  uint runDistance;
-  uint walkDistance;
-  uint yogaDuration;
+  struct Person{
+      string name;
+      uint age;
+      uint height;
+      uint weight;
+      uint bmi;
+      uint runDistance;
+      uint walkDistance;
+      uint yogaDuration;
+      
+  }
+  
+  
   
   function trackRun(uint distance) public returns(uint) {
       runDistance = distance;
